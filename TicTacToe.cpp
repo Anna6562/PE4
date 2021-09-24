964798b92dfaa836ca70adca4c0a60e14dcc585b
Vasr6212 <vasr6212@colorado.edu>
2021-09-24 13:16:20 -0600

    Added my display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,13 +13,13 @@ void CreateBoard(std::vector<std::vector<std::string>>&matrix){
 
 }
 
-void DisplayBoard(std::vector<std::vector<std::string>>&matrix){
-  for (int i = 0; i < matrix.size(); i++){
-      for (int j = 0; j < board[i].size(); j++){
-          std::cout << board[i][j] << " | ";
-      }
-      std::cout << std::endl;
+void DisplayBoard(char** board){
+  for(int x=0;x<3;x++){  // loop through x cordinates
+    for(int y=0;y<3;y++){  // loop for through y cordinates
+      std::cout<<board[x][y];  // display the current value in each corrdinate of the board
     }
+	 std::cout << std::endl;
+  }
 }
 int main(){
   std::vector<std::vector<std::string>> board;
